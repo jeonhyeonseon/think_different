1eda7a16fc076e1a71e5f5eb5ba2605010dc5476
jeonhyeonseon <good_pretty@naver.com>
2025-08-06 16:51:56 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/think_different/think_different/ThinkDifferentApplication.java
@@ -0,0 +1,13 @@
+package com.think_different.think_different;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkDifferentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkDifferentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/think_different/think_different/ThinkDifferentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.think_different.think_different;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkDifferentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
