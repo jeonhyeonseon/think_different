615501b96e17c03e6070b6fe7fff7c676fcdc559
jeonhyeonseon <good_pretty@naver.com>
2025-09-09 18:24:59 +0900

    feat: 채팅 엔티티 클래스 기능 추가

--- src/main/java/com/think_different/think_different/ThinkDifferentApplication.java
+++ src/main/java/com/think_different/think_different/ThinkDifferentApplication.java
@@ -2,8 +2,10 @@ package com.think_different.think_different;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ThinkDifferentApplication {
 
     public static void main(String[] args) {
