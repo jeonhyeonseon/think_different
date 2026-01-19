92ed184130c1cf34034b2e0d00b58b14125a9f01
jeonhyeonseon <good_pretty@naver.com>
2026-01-19 14:39:23 +0900

    feat: Spring Security 접근 권한 및 로그인 설정 추가

---
+++ src/main/java/com/think_different/think_different/config/webSecurity/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.think_different.think_different.config.webSecurity;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, ApplicationEventPublisher applicationEventPublisher) throws Exception {
+        /**
+         * - permitAll() : 모두 허용
+         * - authenticated() : 인증된 사용자만 허용
+         * - anonymous() : 인증하지 않은 사용자만 허용
+         * - hasRole(), hasAnyRole() : 특정 권한이 있는 사용자만 허용
+         */
+
+        http.authorizeHttpRequests((registry) -> {
+            registry
+                    .requestMatchers( // permitAll
+                            "/",
+                            "/members/join",      // 회원가입 화면
+                            "/members",           // 회원가입 POST (컨트롤러가 @PostMapping 이면 여기)
+                            "/members/login",     // 커스텀 로그인 화면
+                            "/board",
+                            "/chatrooms"
+                    ).permitAll()
+                    .requestMatchers( // authenticated
+                            "/chatrooms/*/chat",
+                            "/chatrooms/*/edit",
+                            "/chatrooms/*/delete",
+                            "/board/**"
+                    ).authenticated()
+                    .anyRequest().authenticated();
+        });
+
+        http.formLogin(configurer -> {
+           configurer.loginPage("/members/login")
+                   .usernameParameter("loginId")
+                   .passwordParameter("password")
+                   .defaultSuccessUrl("/chatrooms", true)
+                   .permitAll();
+        });
+
+        // 로그아웃 - POST 요청만 가능
+        http.logout(configurer -> {
+            configurer.logoutUrl("/members/logout")
+                    .logoutSuccessUrl("/")
+                    .permitAll();
+        });
+
+        return http.build();
+    }
+
+    // 비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 로그인 후 리다리렉트 경로 설정
+
+}
