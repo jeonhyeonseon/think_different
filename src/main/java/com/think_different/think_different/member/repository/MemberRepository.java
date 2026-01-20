fb490a493b656ac80ce76cf322935d7f777b0c90
jeonhyeonseon <good_pretty@naver.com>
2026-01-20 15:07:13 +0900

    feat: 아이디/이메일 중복체크 기능 추가

--- src/main/java/com/think_different/think_different/member/repository/MemberRepository.java
+++ src/main/java/com/think_different/think_different/member/repository/MemberRepository.java
@@ -3,6 +3,16 @@ package com.think_different.think_different.member.repository;
 import com.think_different.think_different.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
+
+    // 아이디 중복 체크
+    boolean existsByLoginId(String loginId);
+
+    // 이메일 중복 체크
+    boolean existsByEmail(String email);
 }
