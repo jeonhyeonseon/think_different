902b7ae25c7f959bb36d4c08b59b148be19aa2c4
jeonhyeonseon <good_pretty@naver.com>
2026-01-22 17:51:02 +0900

    feat: 카테고리/타입 추가

---
+++ src/main/java/com/think_different/think_different/expense/domain/Category.java
@@ -0,0 +1,24 @@
+package com.think_different.think_different.expense.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum Category {
+
+    FOOD("FOOD", "음식"), // 음식
+    CAFE("CAFE", "카페"), // 카페
+    TRAFFIC("TRAFFIC", "교통"), // 교통
+    TELECOM("TELECOM", "통신"), // 통신
+    OTT("OTT", "OTT"),
+    INSURANCE("INSURANCE", "보험"), // 보험
+    SAVINGS("SAVINGS", "적금"), // 적금
+    INVEST("INVEST", "투자"); // 투자
+
+    private String categoryName;
+    private String displayCategoryName;
+
+    Category(String categoryName, String displayCategoryName) {
+        this.categoryName = categoryName;
+        this.displayCategoryName = displayCategoryName;
+    }
+}
