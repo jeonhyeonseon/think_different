ea0e0c782275b6ba0bb0431045c2414bb6626edb
jeonhyeonseon <good_pretty@naver.com>
2026-02-12 18:54:21 +0900

    update: 카테고리 추가

--- src/main/java/com/think_different/think_different/expense/domain/Category.java
+++ src/main/java/com/think_different/think_different/expense/domain/Category.java
@@ -9,10 +9,12 @@ public enum Category {
     CAFE("CAFE", "카페"), // 카페
     TRAFFIC("TRAFFIC", "교통"), // 교통
     TELECOM("TELECOM", "통신"), // 통신
+    HOSPITAL("HASPITAL", "병원"), // 병원
     OTT("OTT", "OTT"),
     INSURANCE("INSURANCE", "보험"), // 보험
     SAVINGS("SAVINGS", "적금"), // 적금
-    INVEST("INVEST", "투자"); // 투자
+    INVEST("INVEST", "투자"), // 투자
+    ETC("ETC", "기타");
 
     private String categoryName;
     private String displayCategoryName;
