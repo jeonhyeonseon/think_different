1b4508b35058cdc1e4526380aa459fab66f8b285
jeonhyeonseon <good_pretty@naver.com>
2026-07-09 09:03:30 +0900

    refactor: 캘린더 입력 검증 적용 #104

--- src/main/java/com/think_different/think_different/calendar/dto/CalendarRequestDto.java
+++ src/main/java/com/think_different/think_different/calendar/dto/CalendarRequestDto.java
@@ -4,6 +4,9 @@ import com.think_different.think_different.calendar.entity.Calendar;
 import com.think_different.think_different.calendar.entity.CalendarCategory;
 import com.think_different.think_different.couple.domain.Couple;
 import com.think_different.think_different.member.entity.Member;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -13,8 +16,15 @@ import java.time.LocalTime;
 @Data
 public class CalendarRequestDto {
 
+    @NotBlank(message = "일정을 입력해주세요.")
+    @Size(max = 20, message = "일정은 20자 미만으로 입력해주세요.")
+    @Pattern(
+            regexp = "^(?=.*[가-힣a-zA-Z])[가-힣a-zA-Z0-9\\s]+$",
+            message = "일정은 한글 또는 영어를 포함해야 하며, 숫자만 입력할 수 없습니다."
+    )
     private String title;
     private CalendarCategory category;
+    @Size(max = 300, message = "메모는 300자 이하로 입력해주세요.")
     private String memo;
     private LocalDate scheduleDate;
     private LocalTime startTime;
